1e8b9a559fbdf54d0c5a641ac71128ae8801a863
angrylid <zm4399@gmail.com>
2021-11-23 11:49:49 +0800

    sql of finding user.

--- src/test/java/io/github/angrylid/mall/TestRegister.java
+++ src/test/java/io/github/angrylid/mall/TestRegister.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.angrylid.mall.mapper.AccountMapper;
 import io.github.angrylid.mall.mapper.CustomUserMapper;
 import io.github.angrylid.mall.mbg.entity.User;
 
@@ -16,10 +17,25 @@ public class TestRegister {
     @Autowired
     private CustomUserMapper customUserMapper;
 
+    @Autowired
+    private AccountMapper accountMapper;
+
     @Test
     public void testSelect() {
         List<User> userList = customUserMapper.selectList(null);
         assertEquals(22, userList.size());
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void findDeveloper() {
+        List<Long> idList = accountMapper.getUserIdByTelephone("17132954889");
+        assertEquals(10001L, idList.get(0));
+    }
+
+    @Test
+    public void findFake() {
+        List<Long> idList = accountMapper.getUserIdByTelephone("17132954888");
+        assertEquals(10002L, idList.get(0));
     }
 }
