4c1a7d88a1b8fb1a0926d1016a23765269782805
angrylid <zm4399@gmail.com>
2022-01-08 11:03:45 +0800

    Write some test without exceptions unresolved.

--- src/main/java/io/github/angrylid/mall/generated/entity/Product.java
+++ src/main/java/io/github/angrylid/mall/generated/entity/Product.java
@@ -3,6 +3,7 @@ package io.github.angrylid.mall.generated.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * <p>
@@ -23,17 +24,7 @@ public class Product implements Serializable {
 
     private String description;
 
-    private Integer price;
-
-    private String image0;
-
-    private String image1;
-
-    private String image2;
-
-    private String image3;
-
-    private String image4;
+    private BigDecimal price;
 
 
     public Integer getId() {
@@ -60,52 +51,12 @@ public class Product implements Serializable {
         this.description = description;
     }
 
-    public Integer getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(Integer price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public String getImage0() {
-        return image0;
-    }
-
-    public void setImage0(String image0) {
-        this.image0 = image0;
-    }
-
-    public String getImage1() {
-        return image1;
-    }
-
-    public void setImage1(String image1) {
-        this.image1 = image1;
-    }
-
-    public String getImage2() {
-        return image2;
-    }
-
-    public void setImage2(String image2) {
-        this.image2 = image2;
-    }
-
-    public String getImage3() {
-        return image3;
-    }
-
-    public void setImage3(String image3) {
-        this.image3 = image3;
-    }
-
-    public String getImage4() {
-        return image4;
-    }
-
-    public void setImage4(String image4) {
-        this.image4 = image4;
     }
 
     @Override
@@ -115,11 +66,6 @@ public class Product implements Serializable {
         ", title=" + title +
         ", description=" + description +
         ", price=" + price +
-        ", image0=" + image0 +
-        ", image1=" + image1 +
-        ", image2=" + image2 +
-        ", image3=" + image3 +
-        ", image4=" + image4 +
         "}";
     }
 }
--- src/test/java/io/github/angrylid/mall/mapper/ProductInsertionTest.java
+++ src/test/java/io/github/angrylid/mall/mapper/ProductInsertionTest.java
@@ -1,24 +1,57 @@
 package io.github.angrylid.mall.mapper;
 
-import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import io.github.angrylid.mall.generated.entity.Product;
 import io.github.angrylid.mall.generated.mapper.ProductMapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@MybatisPlusTest
+@SpringBootTest
 public class ProductInsertionTest {
     @Autowired
     private ProductMapper productMapper;
 
+    StringBuilder stringBuilder;
+
+    String title;
+
+    String description;
+
+    @BeforeEach
+    void testStringBuilder() {
+        stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            stringBuilder.append("1234567890");
+        }
+
+        title = stringBuilder.toString();
+
+        for (int i = 0; i < 45; i++) {
+            stringBuilder.append("!@#$%^&*()");
+        }
+
+        description = stringBuilder.toString();
+        // assertThat(title.length()).isEqualTo(500);
+
+    }
+
     @Test
     void testInsert() {
         var product = new Product();
+
+        product.setTitle(title);
+        product.setDescription(description);
+        product.setPrice(new BigDecimal(12.123456));
+
         productMapper.insert(product);
-        assertEquals(2, product.getId());
+        assertThat(product.getId()).isNotNull();
     }
 
 }
