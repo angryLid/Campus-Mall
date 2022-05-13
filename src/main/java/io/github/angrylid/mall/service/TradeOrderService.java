f8c624104fda30b09e6787edad7109bea5938549
angrylid <zm4399@gmail.com>
2022-05-14 01:09:31 +0800

    修复：清空购物车

--- src/main/java/io/github/angrylid/mall/service/TradeOrderService.java
+++ src/main/java/io/github/angrylid/mall/service/TradeOrderService.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 
 import io.github.angrylid.mall.dto.request.OrderDTO;
 import io.github.angrylid.mall.entity.DetailOrder;
+import io.github.angrylid.mall.generated.entity.Cart;
 import io.github.angrylid.mall.generated.entity.Product;
 import io.github.angrylid.mall.generated.entity.TradeOrder;
+import io.github.angrylid.mall.generated.mapper.CartMapper;
 import io.github.angrylid.mall.generated.mapper.ProductMapper;
 import io.github.angrylid.mall.generated.mapper.TradeOrderMapper;
 import io.github.angrylid.mall.mapper.DetailOrderMapper;
@@ -21,14 +23,15 @@ public class TradeOrderService {
     private TradeOrderMapper tradeOrderMapper;
     private ProductMapper productMapper;
     private DetailOrderMapper detailOrderMapper;
+    private CartMapper cartMapper;
 
     @Autowired
     public TradeOrderService(TradeOrderMapper tradeOrderMapper, ProductMapper productMapper,
-            DetailOrderMapper detailOrderMapper) {
+            DetailOrderMapper detailOrderMapper, CartMapper cartMapper) {
         this.tradeOrderMapper = tradeOrderMapper;
         this.productMapper = productMapper;
         this.detailOrderMapper = detailOrderMapper;
-
+        this.cartMapper = cartMapper;
     }
 
     public List<DetailOrder> selectSold(Integer userId) {
@@ -42,7 +45,8 @@ public class TradeOrderService {
     public Integer insertOrders(Integer userId, List<OrderDTO> orders) {
 
         for (OrderDTO order : orders) {
-            Product product = productMapper.selectOne(new QueryWrapper<Product>().eq("id", order.getId()));
+            Cart cart = cartMapper.selectOne(new QueryWrapper<Cart>().eq("id", order.getId()));
+            Product product = productMapper.selectOne(new QueryWrapper<Product>().eq("id", cart.getProductId()));
             TradeOrder tradeOrder = new TradeOrder();
             tradeOrder.setSellerId(product.getSellerId());
             tradeOrder.setConsumerId(userId);
